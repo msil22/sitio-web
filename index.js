603831097008ca44e02bada057b7c4ea0a3f95e9
Matias Silva <msil.igh@gmail.com>
2024-01-23 12:47:24 -0300

    primer avance

---
+++ index.js
@@ -0,0 +1,19 @@
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let nombre = document.querySelector("input[name='nombre']").value;
+    let correo = document.querySelector("input[name='correo']").value;
+    let mensaje = document.querySelector("input[name='mensaje']").value;
+    // Enviar el correo electrónico
+    let email = new Email({
+    to: "tu@email.com",
+    subject: "Mensaje de contacto",
+    body: `
+        De: ${nombre}
+        Correo electrónico: ${correo}
+        Mensaje:
+        ${mensaje}
+    `,
+    });
+    email.send();
+    alert("¡Mensaje enviado con éxito!");
+});
